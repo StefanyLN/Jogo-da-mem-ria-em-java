cb3720ab662b3cf0cee8718b363289d8dc65be55
StefanyLN <54037591+StefanyLN@users.noreply.github.com>
2020-11-20 16:19:23 -0300

    Create TocadorSom.java

---
+++ Memoria/código/TocadorSom.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memoria;
+
+import java.io.File;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+
+public class TocadorSom {
+    
+        String  path;
+
+
+    public void tocaSom(String strCaractere){
+   
+       
+             try{
+                Clip clip = AudioSystem.getClip();
+                clip.open(AudioSystem.getAudioInputStream(new File(strCaractere)));
+                clip.start();
+             
+             }catch (Exception exc){
+                exc.printStackTrace(System.out);
+             }
+             
+   
+    }
+}

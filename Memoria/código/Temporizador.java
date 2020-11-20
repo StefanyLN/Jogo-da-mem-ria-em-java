7d9bc38e377faa86ab6fa4b0b615922c117609fd
StefanyLN <54037591+StefanyLN@users.noreply.github.com>
2020-11-20 16:18:44 -0300

    Create Temporizador.java

---
+++ Memoria/código/Temporizador.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memoria;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Temporizador implements ActionListener {
+    
+    int status =0;
+    
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    
+    public int mudaStatus(){
+        return 3;
+    }
+
+
+}
+
